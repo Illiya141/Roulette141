05e9ecb0460fa18dc88be4ee509f40baf1c0373d
Illiya141 <illiya121@gmail.com>
2023-12-27 01:52:25 +0100

    try again

--- backennd/index.js
+++ backennd/index.js
@@ -1,18 +1,88 @@
-const WebSocketServer = require('ws').Server;
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-const wss = new WebSocketServer({ port: 8080});
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+const port = 3000;
 
-wss.on('connection', (ws)=> {
-    console.log('Incoming connection...');
+app.use(bodyParser.json());
+
+const userBalance = {};
+
+const broadcast = (data) => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(data));
+    }
+  });
+};
+
+// Serve static files from the specified directory
+const staticFilesPath = '/Users/illiyaroshangah/Documents/roulette/Some-Roulette-game/frontend';
+app.use(express.static(staticFilesPath));
+
+app.get('/balance/:userID', (req, res) => {
+  const userID = req.params.userID;
+  const balance = userBalance[userID];
+  res.json({ userID, balance });
 });
 
+app.post('/transaction', (req, res) => {
+  const { userID, amount, type } = req.body;
 
-setInterval(()=> {
-    const computed_random_number = Math.floor(Math.random() * 14);
+  if (!userID || !amount || !type) {
+    return res.status(400).json({ error: 'Missing required parameters' });
+  }
 
-    wss.clients.forEach((ws) => {
-        if(ws.readyState === ws.OPEN){
-            ws.send(computed_random_number);
-        }
-    })
+  if (type === 'deposit') {
+    userBalance[userID] = (userBalance[userID] || 0) + amount;
+  } else if (type === 'withdraw') {
+    const currentBalance = userBalance[userID] || 0;
+    if (currentBalance < amount) {
+      return res.status(400).json({ error: 'Insufficient balance' });
+    }
+    userBalance[userID] -= amount;
+  } else {
+    return res.status(400).json({ error: 'Invalid transaction type' });
+  }
+
+  broadcast({ type: 'balanceUpdate', userId: userID, balance: userBalance[userID] });
+
+  res.json({ success: true });
+});
+
+// WebSocket connection handling
+wss.on('connection', (ws) => {
+  ws.send(JSON.stringify({ type: 'initialBalances', balances: userBalance }));
+  
+  ws.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+wss.on('connection', (ws) => {
+  console.log('Incoming connection...');
+});
+
+// Route for /roulette
+app.get('/roulette', (req, res) => {
+  res.sendFile(path.join(staticFilesPath, 'index.html'));
+});
+
+setInterval(() => {
+  const computed_random_number = Math.floor(Math.random() * 14);
+
+  wss.clients.forEach((ws) => {
+    if (ws.readyState === ws.OPEN) {
+      ws.send(computed_random_number);
+    }
+  });
 }, 10000);
--- frontend/script.js
+++ frontend/script.js
@@ -1,18 +1,32 @@
+
 $(document).ready(function() {
-	//setup multiple rows of colours, can also add and remove while spinning but overall this is easier.
-	initWheel();
- 
-    const socket = new WebSocket('ws://localhost:8080');
-    socket.addEventListener('message', (event) =>{
-        const num = parseInt(event.data);
-        spinWheel(num)
-    });
+  // Initialize user balance to 1000
+  let userBalance = 1000;
+  updateBalanceOnSite(userBalance);
+
+  // Setup multiple rows of colors, can also add and remove while spinning but overall this is easier.
+  initWheel();
+
+  const socket = new WebSocket('ws://localhost:8080');
+  socket.addEventListener('message', (event) => {
+      const num = parseInt(event.data);
+      spinWheel(num);
+      // Update user balance based on the game logic if needed
+      userBalance += someLogicToCalculateBalanceChange(num);
+      updateBalanceOnSite(userBalance);
+  });
 });
 
-function initWheel(){
-	var $wheel = $('.roulette-wrapper .wheel'),
-  		row = "";
-      
+function updateBalanceOnSite(balance) {
+  const userBalanceElement = $('#userBalance');
+  userBalanceElement.text(`User Balance: ${balance}`);
+}
+
+
+function initWheel() {
+  var $wheel = $('.roulette-wrapper .wheel'),
+      row = "";
+
   row += "<div class='row'>";
   row += "  <div class='card red'>1<\/div>";
   row += "  <div class='card black'>14<\/div>";
@@ -30,50 +44,44 @@ function initWheel(){
   row += "  <div class='card red'>7<\/div>";
   row += "  <div class='card black'>8<\/div>";
   row += "<\/div>";
-  
-	for(var x = 0; x < 29; x++){
-  	$wheel.append(row);
+
+  for (var x = 0; x < 29; x++) {
+      $wheel.append(row);
   }
 }
 
-function spinWheel(roll){
+function spinWheel(roll) {
   var $wheel = $('.roulette-wrapper .wheel'),
-  		order = [0, 11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4],
+      order = [0, 11, 5, 10, 6, 9, 7, 8, 1, 14, 2, 13, 3, 12, 4],
       position = order.indexOf(roll);
-            
-  //determine position where to land
+
+  // Determine position where to land
   var rows = 12,
-  		card = 75 + 3 * 2,
+      card = 75 + 3 * 2,
       landingPosition = (rows * 15 * card) + (position * card);
-  	
-  var randomize = Math.floor(Math.random() * 75) - (75/2);
-    
+
+  var randomize = Math.floor(Math.random() * 75) - (75 / 2);
+
   landingPosition = landingPosition + randomize;
-    
+
   var object = {
-		x: Math.floor(Math.random() * 50) / 100,
-    y: Math.floor(Math.random() * 20) / 100
-	};
-  
+      x: Math.floor(Math.random() * 50) / 100,
+      y: Math.floor(Math.random() * 20) / 100
+  };
+
   $wheel.css({
-		'transition-timing-function':'cubic-bezier(0,'+ object.x +','+ object.y + ',1)',
-		'transition-duration':'6s',
-		'transform':'translate3d(-'+landingPosition+'px, 0px, 0px)'
-	});
-  
-  setTimeout(function(){
-		$wheel.css({
-			'transition-timing-function':'',
-			'transition-duration':'',
-		});
-    
-    var resetTo = -(position * card + randomize);
-		$wheel.css('transform', 'translate3d('+resetTo+'px, 0px, 0px)');
+      'transition-timing-function': 'cubic-bezier(0,' + object.x + ',' + object.y + ',1)',
+      'transition-duration': '6s',
+      'transform': 'translate3d(-' + landingPosition + 'px, 0px, 0px)'
+  });
+
+  setTimeout(function() {
+      $wheel.css({
+          'transition-timing-function': '',
+          'transition-duration': '',
+      });
+
+      var resetTo = -(position * card + randomize);
+      $wheel.css('transform', 'translate3d(' + resetTo + 'px, 0px, 0px)');
   }, 6 * 1000);
 }
-
-
-
-
-
-
